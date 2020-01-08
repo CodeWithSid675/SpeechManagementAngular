dfadd999abd65774448f9c2c745d844125b4057e
Siddharth Mishra <sidmishra675@gmail.com>
2020-01-08 17:55:06 +0530

    second commit for constant for backend api

--- src/app/commonFuntions/constants.ts
+++ src/app/commonFuntions/constants.ts
@@ -1,5 +1,7 @@
 export class Constants{
     public static get BASE_URL(): string { return window.location.origin; };
-    public static get JOSN_DB_URL(): string { return "http://localhost:8080/speeches" };
+    // public static get JOSN_DB_URL(): string { return "http://localhost:1337/speeches" };// for node api 
+    public static get JOSN_DB_URL(): string { return "http://localhost:8080/speeches" };//for spring api
+    
 
 }
